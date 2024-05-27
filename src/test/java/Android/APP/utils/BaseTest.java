41b1f8772cb2234ed697217306952acae5e9a043
Yassmin-gamal <66153260+Yassmin-gamal@users.noreply.github.com>
2024-05-27 17:52:43 +0300

    Update

--- src/test/java/Android/APP/utils/BaseTest.java
+++ src/test/java/Android/APP/utils/BaseTest.java
@@ -23,12 +23,14 @@ public class BaseTest {
     public void setUp() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "SM A145F"); // Change to your device name
-        caps.setCapability("udid", "RK8W5048BXB");
-        caps.setCapability("plarformVersion", "13.0");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "SM A145F"); 
+        caps.setCapability(MobileCapabilityType.UDID, "RK8W5048BXB");
+        caps.setCapability( MobileCapabilityType.PLATFORM_VERSION, "13.0");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        
         caps.setCapability("appPackage","com.sec.android.app.popupcalculator");
 	    caps.setCapability("appActivity","com.sec.android.app.popupcalculator.Calculator");
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+         
         System.out.println("System Started......");
 
         driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
